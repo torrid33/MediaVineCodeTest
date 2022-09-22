22af079b15d3e9bd3560887f522f589c1c405115
torrid33 <tturner@torrid-tech.com>
2022-09-22 13:40:31 -0400

    Clarified code Made it clear this is just a simple example someone asked me to post

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,40 @@
 import "./styles.css";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 export default function App() {
   const [books, setBooks] = useState(null);
 
-   // + adding the use
-    useEffect(() => {
-    getData();
-
+  // + adding the use
+  useEffect(() => {
     // we will use async/await to fetch this data
     async function getData() {
-      const response = await fetch("https://www.anapioficeandfire.com/api/books");
+      const response = await fetch(
+        "https://www.anapioficeandfire.com/api/books"
+      );
       const data = await response.json();
 
       // store the data into our books variable
-      setBooks(data) ;
+      setBooks(data);
     }
-  }, []); // <- you may need to put the setBooks function in this array
 
+    getData();
+  }, []); //
 
   return (
     <div className="App">
-      <h1>GitHub Issues Code Challenge</h1>
-      <h2>for MediaVine</h2>
-
-        {/* display books from the API */}
-        {books && (
-      <div className="books" align="left">
-
-        {/* loop over the books */}
-        {books.map((book, index) => (
-          <div key={index}>
-            <h2>{book.name}</h2>
-          </div>
-        ))}
-
-      </div>
-    )}
-
+      <h1>Data Fetch Sample</h1>
+      (Yes simple example but a recruiter asked me to post)
+      {/* display books from the API */}
+      {books && (
+        <div className="books" align="left">
+          {/* loop over the books */}
+          {books.map((book, index) => (
+            <div key={index}>
+              <h2>{book.name}</h2>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
